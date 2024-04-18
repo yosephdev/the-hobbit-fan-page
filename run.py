d141f179a202036bdc4685f4c7a4882e48de6af2
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-18 16:23:59 +0000

    feat: Update run.py to render index.html template

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Wworld"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
