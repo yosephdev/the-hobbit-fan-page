34a35dec7c2080477e56f22f5b52bd47011601bc
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-18 16:07:27 +0000

    feat: Add Flask application setup and basic route

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, Wworld"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
